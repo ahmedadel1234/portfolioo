b4ba983e280e734ea61113d8ab5c15bb25f6a364
ahmedadel1234 <67295444+ahmedadel1234@users.noreply.github.com>
2023-05-15 11:41:45 +0300

    Update main.js

--- georashad/js/main.js
+++ georashad/js/main.js
@@ -4,10 +4,16 @@
             document.querySelector(
             "body").style.visibility = "hidden";
             document.querySelector(
+            "body").style.height = "100vh";
+            document.querySelector(
+            "body").style.overflowY = "hidden";
+            document.querySelector(
             "#loader").style.visibility = "visible";
         } else {
             document.querySelector(
             "#loader").style.display = "none";
+            document.querySelector(
+                "body").style.overflowY = "scroll";
             document.querySelector(
             "body").style.visibility = "visible";
         }
