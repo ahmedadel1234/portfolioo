68e80d9b50653cf51894c5d4d3edef2c8a1fc0c7
ahmedadel1234 <67295444+ahmedadel1234@users.noreply.github.com>
2023-08-16 14:41:22 +0300

    Add files via upload

--- css/main.js
+++ css/main.js
@@ -1,33 +1,41 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let letter = 0;
-  const text = "Hi, Iam Ahmed Adel Elghiaty";
-  function typeText() {
-    if (letter < text.length) {
-      document.getElementById("type-js").innerHTML += text.charAt(letter);
-      letter++;
-      let speed = Math.floor(Math.random() * 150) + 50;
-      setTimeout(typeText, speed);
-    }
-  }
-  typeText();
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//   let letter = 0;
+//   const text = "Hi, Iam Ahmed Adel Elghiaty";
+//   function typeText() {
+//     if (letter < text.length) {
+//       document.getElementById("type-js").innerHTML += text.charAt(letter);
+//       letter++;
+//       let speed = Math.floor(Math.random() * 150) + 50;
+//       setTimeout(typeText, speed);
+//     }
+//   }
+//   typeText();
+// });
 
-let totopBtn = document.querySelector(".totop");
-let version = document.querySelector(".nupdate");
+// let totopBtn = document.querySelector(".totop");
+// let version = document.querySelector(".nupdate");
 
-totopBtn.onclick = function () {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
-  });
-};
+// totopBtn.onclick = function () {
+//   window.scrollTo({
+//     top: 0,
+//     behavior: "smooth",
+//   });
+// };
 
-window.onscroll = function () {
-  if (window.scrollY >= 600) {
-    totopBtn.style.display = "block";
-  } else {
-    totopBtn.style.display = "none";
-  }
-};
+// window.onscroll = function () {
+//   if (window.scrollY >= 600) {
+//     totopBtn.style.display = "block";
+//   } else {
+//     totopBtn.style.display = "none";
+//   }
+// };
 
-//
+function openpg() {
+  let contactuspg = document.getElementById("contactuspg");
+  contactuspg.style.display = "flex";
+}
+
+function closepg() {
+  let contactuspg = document.getElementById("contactuspg");
+  contactuspg.style.display = "none";
+}
